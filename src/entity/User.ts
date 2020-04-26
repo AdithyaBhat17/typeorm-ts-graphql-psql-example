61ed2a63bf3e1dbb0ab25194500a2b05c056bb56
Adithya NR <adithyabhat17.ab@gmail.com>
2020-04-26 10:21:46 +0530

    🎨 updated user schema

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,18 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { Field, Int, ObjectType } from "type-graphql";
 
-@Entity()
-export class User {
+@ObjectType()
+@Entity("users")
+export class User extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Field()
+  @Column("text", { nullable: false })
+  fullname: string;
 
-    @Column()
-    firstName: string;
-
-    @Column()
-    lastName: string;
-
-    @Column()
-    age: number;
-
+  @Field()
+  @Column("text", { nullable: false, unique: true })
+  email: string;
 }
